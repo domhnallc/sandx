85112c0d4983af3bf6f275df891586a7a9a81632
Domhnall <d.carlin@qub.ac.uk>
2025-07-21 20:35:45 +0100

    updates get_online_tailscale_members with check for empty lines or beginning with #

--- SANDx/tailscale.py
+++ SANDx/tailscale.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def get_tailscale_members():
+def get_online_tailscale_members():
     base_cmd = "tailscale status"
     filter = "grep -v offline"
 
@@ -9,9 +9,19 @@ def get_tailscale_members():
         output = subprocess.check_output(cmd, shell=True, text=True)
         members = []
         for line in output.strip().split('\n'):
-            parts = line.split()
-            if len(parts) > 1:
-                members.append(parts[1])  # Assuming the second part is the member name
+            if len(line) == 0 or line.startswith('#'):
+                pass
+            else:
+                parts = line.split()
+                if len(parts) > 1:
+                    members.append(parts[1])  # Assuming the second part is the member name
         return members
+    except subprocess.CalledProcessError as e:
+        print("error" + e.output)
+        return []
+    
+def main():
+    online_machines = get_online_tailscale_members()
+    print(online_machines)
 
-
+main()
