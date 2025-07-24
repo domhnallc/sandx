8869d0afd44e06063578f36fdfb5e03e8b5016e3
Domhnall <d.carlin@qub.ac.uk>
2025-07-24 10:35:36 +0100

    creates Experiment class to hold run details

--- SANDx/runner.py
+++ SANDx/runner.py
@@ -1,17 +1,42 @@
 from pathlib import Path
 from shutil import copy2
+from dataclasses import dataclass
 
 import process_options as po
 import tailscale as ts
 
 import os
 
-def run_analysis(input_folder, selected_machines, cpu, experiments, output_folder, num_folders, notify='no'):
+# Define the experiment class to hold the parameters for running the analysis
+@dataclass
+class Experiment:
+    
+    def __init__(self, input_folder: Path, selected_machines: list[str], cpu: str, experiments: list[str], output_folder: str, num_folders: int = 1, notify: bool = False):
+        self.input_folder = input_folder
+        self.selected_machines = selected_machines
+        self.cpu = cpu
+        self.experiments = experiments
+        self.output_folder = output_folder
+        self.num_folders = num_folders
+        self.notify = notify
+    
+    def __str__(self):
+        return (f"Experiment(input_folder:{self.input_folder}, "
+                f"selected_machines:{self.selected_machines}, "
+                f"cpu:{self.cpu}, "
+                f"experiments:{self.experiments}, "
+                f"output_folder:{self.output_folder}, "
+                f"num_folders:{self.num_folders}, "
+                f"notify:{self.notify})")
+
+
+
+def run_analysis(exp: Experiment):
     """Runs the analysis on specified machines with given parameters.
     """
-    input_split_folders = split_input_folder(input_folder, num_folders)
-    map_folders_to_machines(num_folders, input_split_folders, selected_machines)
-    send_split_folders_to_machines(num_folders, selected_machines, input_split_folders) 
+    input_split_folders = exp.split_input_folder(exp.input_folder, exp.num_folders)
+    map_folders_to_machines(exp.num_folders, input_split_folders, exp.selected_machines)
+    send_split_folders_to_machines(exp.num_folders, exp.selected_machines, input_split_folders)
 
     for machine in selected_machines:
         run_on_machine(machine, cpu, experiments, output_folder)    
@@ -97,7 +122,16 @@ def main():
     """Main entry point for the script"""
     args = po.get_options()
     po.display_config(args)
-    run_analysis(args.input_folder, args.machines, args.cpu, args.experiments, args.output_folder, args.num_folders)
+    exp = Experiment(   
+        input_folder=Path(args.input_folder),
+        selected_machines=args.machines,
+        cpu=args.cpu,
+        experiments=args.experiments,
+        output_folder=args.output_folder,
+        num_folders=args.num_folders,
+        notify=args.notify
+    )
+    run_analysis(exp))
     
     return args  # Return args for further processing if needed
 
