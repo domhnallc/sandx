d04d65f0a595a1864eee76ced41c2f647b379d66
Domhnall <d.carlin@qub.ac.uk>
2025-08-12 17:26:08 +0100

    fix path

--- SANDx/experiment.py
+++ SANDx/experiment.py
@@ -190,7 +190,7 @@ class Experiment:
         print(f"Running analysis on {machine} with CPU {self.cpu} for experiments {self.experiments}...")
     
 
-        cmd = f"./home/admin2/shared/external_vm_runner.sh -i {split_folder} -c {self.cpu} -e {self.experiments} -o {self.output_folder}"
+        cmd = f"/home/admin2/shared/external_vm_runner.sh -i {split_folder} -c {self.cpu} -e {self.experiments} -o {self.output_folder}"
         print(f"\nCMD: {cmd}\n")
         ts.run_command_on_tailscale_machine(machine, cmd, user='admin2')
 
