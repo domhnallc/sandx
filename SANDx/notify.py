5e24afb12aa6ca5fd4bb6355f5e8320802d7506c
Domhnall <d.carlin@qub.ac.uk>
2025-08-13 16:38:10 +0100

    update notify trace

--- SANDx/notify.py
+++ SANDx/notify.py
@@ -5,7 +5,7 @@ def send_notification(url, topic, message):
     Send a notification message to a specified topic using ntfy.
     """
     re = requests.post(f"{url}/{topic}", data=message.encode(encoding='utf-8'))
-    print(f"Notifying: {re.text}")
+    print(f"Notifying: {url}/{topic} with message: {message}")
 
     return re.status_code
 
