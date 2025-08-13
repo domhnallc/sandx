66a17fa7de70481ea713da96b8a133ff816b03db
Domhnall <d.carlin@qub.ac.uk>
2025-08-13 16:38:59 +0100

    remove slash between url and topic in notify

--- SANDx/notify.py
+++ SANDx/notify.py
@@ -4,8 +4,8 @@ def send_notification(url, topic, message):
     """
     Send a notification message to a specified topic using ntfy.
     """
-    re = requests.post(f"{url}/{topic}", data=message.encode(encoding='utf-8'))
-    print(f"Notifying: {url}/{topic} with message: {message}")
+    re = requests.post(f"{url}{topic}", data=message.encode(encoding='utf-8'))
+    print(f"Notifying: {url}{topic} with message: {message}")
 
     return re.status_code
 
